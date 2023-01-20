b444eaa642a7104229af21e1aac506ff20d518e4
nightbot <109276523+nightbotblaze@users.noreply.github.com>
2023-01-20 14:59:40 +0700

    Add files via upload

---
+++ Galang.sh
@@ -0,0 +1,87 @@
+#!/bin/sh
+
+
+green='\33[0;32m'
+greenlight='\33[32;1m'
+blue='\33[0;36m'
+bluelight='\33[36;1m'
+red='\33[31;1m'
+white='\33[37;1m'
+black='\33[30;1m'
+oren='\33[33;1m'
+yellowlight='\33[1;33m'
+
+clear
+
+toilet -f big -F gay Yono
+echo
+echo $red"============================================"
+echo   $red"Nama Author: Galang Azriel P"
+echo   $red"Kelas: 7.4"
+echo   $red"Sekolah: SMPN 214 JAKARTA TIMUR"
+echo   $red"Catatan: BABAYOOO" 
+echo $red"============================================"
+echo
+echo $oren"TOOLS INI HANYA UNTUK SENANG² JAN DI BAWA SERIUS"
+echo
+echo
+echo
+echo $green"AUTHOR NYA KANGEN ES PAKDEEEE ESKOPIII"
+echo
+echo
+echo $blue"1.Selamat Datang Anak baru"
+echo 
+echo $yellowlight"2.Selamat Untuk Pengurus kelas baru"
+echo
+echo $greenlight"3.Mau Liat Tools Pengurus Kelas?"
+echo
+echo $red"4.Keluar Ngaps"
+echo
+read -p "Opsi ngaps :" bad
+
+if [ $bad = 1 ]
+then
+echo $bluelight"Smg Tidak stres di kelas ini yang penuh Keberisikan, Wajar Kalo kalian cepet pusing"
+sleep 2
+echo $red"Njay Penyemangat anak Baru, Cmn uji coba tools"
+sleep 2
+read -p "Opsi Lurr :" bad
+fi
+
+if [ $bad = 2 ]
+then
+echo $bluelight"Kalian Harus Berjanji Menertibkan Kelas agar menjadi kelas ya nyaman ya"
+sleep 2
+read -p "Opsi lurr :" bad
+fi
+
+if [ $bad = 1 ]
+then
+echo $green"Kalo ketik 1 Harus masukin di input yang pertama lurr"
+sleep 2
+echo $green"Sabar Kack -_-"
+sh Galang.sh
+fi
+
+if [ $bad = 3 ]
+then
+sleep 2
+echo $green"Nih"
+sleep 2
+sh PK.sh
+fi
+
+if [ $bad = 4 ]
+then
+sleep 2
+echo $green"Keluar Ngaps"
+exit
+else
+        sleep 2
+        echo "Bagus gk Ceramah gw?"
+        sleep 2
+        fi
+
+
+
+
---
+++ PK.sh
@@ -0,0 +1,109 @@
+#!/bin/sh
+
+green='\33[0;32m'
+greenlight='\33[32;1m'
+blue='\33[0;36m'
+bluelight='\33[36;1m'
+red='\33[31;1m'
+white='\33[37;1m'
+black='\33[30;1m'
+oren='\33[33;1m'
+yellowlight='\33[1;33m'
+
+clear
+
+toilet -f big -F gay PK   7.4
+echo
+echo $red"============================================"
+echo   $red"Nama Author: Galang Azriel P"
+echo   $red"Kelas: 7.4"
+echo   $red"Sekolah: SMPN 214 JAKARTA TIMUR"
+echo   $red"Catatan: BABAYOOO"
+echo $red"============================================"
+echo
+                                echo $green======================================================
+echo
+                                    echo $red"Nama Wali Kelas : Bu Fajar panca indriya Utami s.kom"
+echo
+                                echo $green======================================================
+echo
+echo
+echo
+echo
+echo $white—————————————————————————————————————————————————————————————————————————
+echo                        $red"Ketua & wakil Kelas"
+echo
+echo $yellowlight"Ketua Kelas : Syifa Maharani Jasmine"
+echo "Wakil Ketua : Karina Permata Harimurti"
+echo
+echo $white—————————————————————————————————————————————————————————————————————————
+echo
+echo $white—————————————————————————————————————————————————————————————————————————
+echo                        $red"Sekretaris"
+echo
+echo $green"Sekretaris 1 : Yohana Victoria Sasono"
+echo "Sekretaris 2 : Nadya ayu lestari"
+echo
+echo $white—————————————————————————————————————————————————————————————————————————
+echo
+echo $white—————————————————————————————————————————————————————————————————————————
+echo                        $red"Bendahara"
+echo
+echo $blue"Bendahara 1 : Siti Amira Azahra"
+echo "Bendahara 2 : Mutia Septianti"
+echo
+echo $white—————————————————————————————————————————————————————————————————————————
+echo 
+echo $white—————————————————————————————————————————————————————————————————————————
+echo                        $red"S.Kebersihan"
+echo
+echo $bluelight"S.Kebersihan 1 : Revan Arkana  Mahardika"
+echo "S.Kebersihan 2 : Nasha Maharani"
+echo
+echo $white—————————————————————————————————————————————————————————————————————————
+echo
+echo $white—————————————————————————————————————————————————————————————————————————
+echo                        $red"S.Kebersihan"
+echo
+echo $greenlight"S.Keamanan 1 : Galang Azriel Pamungkas"
+echo "S.Keamanan 2 : Hafi syah Hadi Satriyo"
+echo
+echo $white—————————————————————————————————————————————————————————————————————————
+echo
+echo $yellowlight"1.Opsi Kedua"
+echo
+echo $yellowlight"2.Exit Keluar Ngaps"
+echo
+echo $yellowlight"3.Pengen ke Tools satunya?"
+echo
+read -p "INPUT :" bad
+
+if [ $bad = 1 ]
+then
+sleep 2
+echo $bluelight"Pergantian PENGURUS KELAS di karnakan banyak pengurus kelas yang tidak bertanggung jawab"
+read -p "Input:" bad 
+fi
+
+if [ $bad = 2 ]
+then
+sleep 2
+echo $green"Keluar Ngaps"
+exit
+fi
+
+if [ $bad = 3 ]
+then
+sleep 2
+echo $green"Nih"
+sleep 2
+sh Galang.sh
+else
+        sleep 2
+        echo "Bagus Kan?"
+        echo "Lu salah masukin input lurr"                                                      sleep 2
+        sleep 2
+        sh Galang.sh
+        fi
+
+
